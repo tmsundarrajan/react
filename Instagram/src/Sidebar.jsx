d6888e0bfd4f5c1f0a135ef9b37689c4a8c0ac11
tmsundarrajan <sundar192114l@gmail.com>
2025-10-06 20:53:31 +0530

    create all other sidebar components

--- Instagram/src/Sidebar.jsx
+++ Instagram/src/Sidebar.jsx
@@ -4,6 +4,14 @@ function Sidebar() {
   return (
     <div>
         <img className="logo-text" src="src\assets\Instagram_text.png" alt="" />
+        <div>Home</div>
+        <div>Search</div>
+        <div>Explore</div>
+        <div>Reels</div>
+        <div>Messages</div>
+        <div>Notifications</div>
+        <div>Create</div>
+        <div>Profile</div>
     </div>
   )
 }
