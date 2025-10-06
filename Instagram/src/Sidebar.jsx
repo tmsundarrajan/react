2cead61bcd50ba930f339434565767176f0e8328
tmsundarrajan <sundar192114l@gmail.com>
2025-10-06 21:19:27 +0530

    add icons using bootstrap

--- Instagram/src/Sidebar.jsx
+++ Instagram/src/Sidebar.jsx
@@ -5,18 +5,18 @@ function Sidebar() {
     <>
     <div>
         <img className="logo-text" src="src\assets\Instagram_text.png" alt="" />
-        <div>Home</div>
-        <div>Search</div>
-        <div>Explore</div>
-        <div>Reels</div>
-        <div>Messages</div>
-        <div>Notifications</div>
-        <div>Create</div>
-        <div>Profile</div>
+        <div><i class="bi bi-house-door"></i>Home</div>
+        <div><i class="bi bi-search"></i>Search</div>
+        <div><i class="bi bi-compass"></i>Explore</div>
+        <div><i class="bi bi-play-btn"></i>Reels</div>
+        <div><i class="bi bi-chat-dots"></i>Messages</div>
+        <div><i class="bi bi-heart"></i>Notifications</div>
+        <div><i class="bi bi-plus-square"></i>Create</div>
+        <div><i class="bi bi-person-circle"></i>Profile</div>
     </div>
     <div>
-        <div>Threads</div>
-        <div>More</div>
+        <div><i class="bi bi-threads"></i>Threads</div>
+        <div><i class="bi bi-list"></i>More</div>
     </div>
     </>
   )
