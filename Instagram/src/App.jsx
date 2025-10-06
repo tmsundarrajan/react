ff5b9888e65140b61fdcf544578a787e1806b07b
tmsundarrajan <sundar192114l@gmail.com>
2025-10-06 20:01:38 +0530

    add bootstrap css using className

--- Instagram/src/App.jsx
+++ Instagram/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function App() {
   return (
-    <div>
+    <div className="d-flex vh-100 bg-secondary">
       <div>sidebar</div>
       <div>feed</div>
       <div>suggestions</div>
