aa89efb6b6f6a88c3306639982667ca063a184c3
tmsundarrajan <sundar192114l@gmail.com>
2025-10-06 20:29:46 +0530

    create new called Sidebar and use in App.jsx

--- Instagram/src/App.jsx
+++ Instagram/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Sidebar from './Sidebar'
 
 function App() {
   return (
     <div className="d-flex vh-100">
-      <div className="w-20">sidebar</div>
+      <div className="w-20"><Sidebar/></div>
       <div className="w-50 bg-info">feed</div>
       <div className="w-30">suggestions</div>
     </div>
