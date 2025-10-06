3b9b71b3c1fd3679a86730d1df5e756c1e3d9655
tmsundarrajan <sundar192114l@gmail.com>
2025-10-06 21:23:37 +0530

    add spacing between icons

--- Instagram/src/Sidebar.jsx
+++ Instagram/src/Sidebar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Sidebar() {
   return (
     <>
-    <div>
+    <div className="d-flex flex-column gap-3">
         <img className="logo-text" src="src\assets\Instagram_text.png" alt="" />
         <div><i class="bi bi-house-door"></i>Home</div>
         <div><i class="bi bi-search"></i>Search</div>
@@ -14,7 +14,7 @@ function Sidebar() {
         <div><i class="bi bi-plus-square"></i>Create</div>
         <div><i class="bi bi-person-circle"></i>Profile</div>
     </div>
-    <div>
+    <div className="d-flex flex-column gap-3">
         <div><i class="bi bi-threads"></i>Threads</div>
         <div><i class="bi bi-list"></i>More</div>
     </div>
