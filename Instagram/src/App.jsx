fb6b804e48eb63de6cac71049ed59c51f70aacf0
tmsundarrajan <sundar192114l@gmail.com>
2025-10-06 20:08:56 +0530

    code updated

--- Instagram/src/App.jsx
+++ Instagram/src/App.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function App() {
   return (
-    <div className="d-flex vh-100 bg-secondary">
+    <div className="d-flex vh-100">
       <div>sidebar</div>
-      <div>feed</div>
+      <div className="w-50 bg-info">feed</div>
       <div>suggestions</div>
     </div>
   )
