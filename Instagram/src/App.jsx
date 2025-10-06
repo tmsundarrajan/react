5c6409a57dab51eeb47788c61815c27ed957a76c
tmsundarrajan <sundar192114l@gmail.com>
2025-10-06 19:43:18 +0530

    create three segments sidebar,feed,suggestions

---
+++ Instagram/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <div>sidebar</div>
+      <div>feed</div>
+      <div>suggestions</div>
+    </div>
+  )
+}
+
+export default App
